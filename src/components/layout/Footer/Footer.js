e9f44b83e82f486f8d7d47392dbddde522dbc882
gabriel.silveira8 <gabriel.silveira20@bol.com.br>
2023-04-16 18:15:36 -0300

    Estudado biblioteca React Icons, que utiliza icones como componentes

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -1,8 +1,23 @@
 import React from 'react'
 import styles from './Footer.module.css'
+import {FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa'
+
 function Footer() {
   return (
-    <footer className={styles.footer}>Rodapé do Curso</footer>
+    <footer className={styles.footer}>
+      <ul className={styles.social_list}>
+        <li>
+          <FaFacebook/>
+        </li>
+        <li>
+          <FaInstagram/>
+        </li>
+        <li>
+          <FaLinkedin/>
+        </li>
+      </ul>
+      <p>Rodapé</p>
+    </footer>
   )
 }
 
